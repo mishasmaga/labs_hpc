9d5e5ead49f4605b9ed4c419a5d2adcde3abe582
Mihail Smaga <Mihail.Smaga@zeppelin.com>
2018-06-12 16:46:15 +0300

    add build1.sh

---
+++ build1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+awk 'BEGIN {xmax=100; xmin=-100; step=200; dx=(xmax-xmin)/step;  for (i = xmin; i < xmax; i+=dx) print i" "i+dx }' > ranges.txt
+g++ integral.cxx -oexe
+rm -rf res
+mkdir res
+rm -rf out
+mkdir out
+qsub ./build_job.pbs -t 1-10
