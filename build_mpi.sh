1962d123d46abe05d2af56654c6cdb61f4e304ad
Mihail Smaga <Mihail.Smaga@zeppelin.com>
2018-06-12 22:55:38 +0300

    add -t paramether 1-1

--- build_mpi.sh
+++ build_mpi.sh
@@ -6,5 +6,5 @@ ml icc openmpi
 mpicxx integral_mpi.cxx -std=c++11
 
 #run mpi
- z=$(qsub -v total=100 mpi.sh );  
+ z=$(qsub -v total=100 mpi.sh -t 1-1 );  
  qsub -W depend=afterokarray:$z calc_total_mpi.pbs
