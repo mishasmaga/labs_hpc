d6fbfbafbbe224de0feeff18b6cc2eea19aac22c
Mihail Smaga <Mihail.Smaga@zeppelin.com>
2018-06-14 11:47:34 +0300

    replace 20

--- build_mpi.sh
+++ build_mpi.sh
@@ -6,6 +6,6 @@ ml icc openmpi
 mpicxx integral_mpi.cxx -std=c++11
 
 #run mpi
-qsub -v total=100 mpi.sh
- #z=$(qsub -v total=100 mpi.sh -t 1-1 );  
+qsub -v total=20 mpi.sh
+ #z=$(q sub -v total=100 mpi.sh -t 1-1 );  
  #qsub -W depend=afterokarray:$z calc_total_mpi.pbs
